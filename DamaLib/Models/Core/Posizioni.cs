17967d9b09f3692112e1b01040a414603abd5c53
Lorenzo Brivio <lorenzo.brivio9908@gmail.com>
2019-12-17 18:45:11 +0100

    Implementata una struct per le coordinate

--- DamaLib/Models/Core/Posizioni.cs
+++ DamaLib/Models/Core/Posizioni.cs
@@ -4,18 +4,24 @@ namespace DamaLib.Models.Core
 {
     public static class Posizioni
     {
+        public struct Coordinate
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
         public static int PosFromIndexes(int x, int y)
         {
             if (x < 0 || x >= 8 || y < 0 || y >= 8)
                 throw new Exception("Indice/i non valido/i");
 
-            int X = y%2 == 1 ? x-1 : x;
+            int X = y % 2 == 1 ? x - 1 : x;
             X /= 2;
             int Y = y * 4 + 1;
 
             return Y + X;
         }
-        public static Tuple<int, int> IndexesFromPos(int pos)
+        public static Coordinate IndexesFromPos(int pos)
         {
             if (pos < 1 || pos > 32)
                 throw new Exception("Posizione non valida");
@@ -24,10 +30,10 @@ namespace DamaLib.Models.Core
             int y = (pos - 1) / 4;
 
             // Colonna
-            int tempX = ((pos -1) % 4) * 2;
-            int x = y%2 == 1 ? tempX+1 : tempX;
+            int tempX = ((pos - 1) % 4) * 2;
+            int x = y % 2 == 1 ? tempX + 1 : tempX;
 
-            return new Tuple<int, int>(x,y);
+            return new Coordinate() { X = x, Y = y };
         }
     }
 }
