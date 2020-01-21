2efbb3a9c1e5b4974c76a1b38020504501384605
Lorenzo Brivio <lorenzo.brivio9908@gmail.com>
2020-01-21 13:56:05 +0100

    SALVATAGGIO della prima improntazione della classe

---
+++ DamaLib/Models/Mossa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DamaLib.Models.Core;
+
+namespace DamaLib.Models
+{
+    class Mossa
+    {
+        public bool Turno { get; set; }
+        public Coordinate From { get; set; }
+        public Coordinate To { get; set; }
+    }
+}
