7359682dc6a7682abf6105020ea0a90a912141e5
Lorenzo Brivio <lorenzo.brivio9908@gmail.com>
2020-02-04 12:23:46 +0100

    Added Constants class

---
+++ DamaLib/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DamaLib
+{
+    public static class Constants
+    {
+        public static int DamaServerPort = 55555;
+    }
+}
