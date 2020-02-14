b4d321fb82c2ad014abd64d1104039fb1a812c8e
Lorenzo Brivio <lorenzo.brivio9908@gmail.com>
2020-02-14 17:31:37 +0100

    testato il metodo FindPossiblePlayerMooves()

---
+++ Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DamaLib.Models;
+using DamaLib.Models.Core;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scacchiera s = new Scacchiera();
+
+            Console.WriteLine("Mosse disponibili del giocatore " + (s.Turno ? "bianco" : "nero"));
+            var ls = s.FindPossiblePlayerMooves();
+            foreach (var m in ls)
+            {
+                Console.WriteLine($"({m.From})->({m.To})");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
